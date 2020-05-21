180d17e5eac55464acaed95bb9b74c4925f71290
unknown <ts-sistemas@hotmail.com>
2020-05-21 19:15:33 -0300

    Modificaciones en las Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,34 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+//COMPONENTES
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 
 const routes: Routes = [
-  // ESTAS RUTAS HIJAS SE VAN A CARGAR EN EL ROUTER-OUTLET DEL PAGECOMPONENT QUE ES EL PADRE
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-        {path: 'progress', component: ProgressComponent },
-        {path: 'graficas1', component: Graficas1Component },
-        {path: 'dashboard', component: DashboardComponent },
-        {path: '', redirectTo: '/dashboard', pathMatch: 'full'}
-    ]
-  },
   {path: 'login', component: LoginComponent },
   {path: 'register', component: RegisterComponent },
-  {path: '**', component: NopagefoundComponent }
+  {path: '**', component: NopagefoundComponent },
 
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {useHash:true})],
-  exports: [RouterModule]
+ exports: [RouterModule]
 })
 export class AppRoutingModule { }
